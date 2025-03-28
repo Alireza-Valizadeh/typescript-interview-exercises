8e04a13a2cd588e748cd643eb98b6073fc257abb
Alireza Valizadeh <alirezavalizadeh1999@gmail.com>
2025-03-28 17:06:00 +0330

    feat: add reverse string methods

---
+++ Strings/reverseString.ts
@@ -0,0 +1,19 @@
+/*
+Interviewer:
+Please write a function that reverses any given strings.
+*/
+
+function reverseString_1(input: string): string {
+  return input.split("").reverse().join("");
+}
+
+function reverseString_2(input: string): string {
+  const reversedArr: string[] = [];
+  for (let char of input) {
+    reversedArr.unshift(char);
+  }
+  return reversedArr.join("");
+}
+
+
+console.log(reverseString_2("Behnoosh Jaberi"));
